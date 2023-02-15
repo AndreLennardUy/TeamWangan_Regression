07b230ca95c7c610978773033e314411a275935e
Andre Lennard S. Uy <andrelennard.uy@cit.edu>
2023-02-15 12:22:47 +0800

    Created the UI for BP Ap

--- Regression/Regression/Form1.Designer.cs
+++ Regression/Regression/Form1.Designer.cs
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ageIn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.weightIn = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(64, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Regression Application";
+            // 
+            // ageIn
+            // 
+            this.ageIn.Location = new System.Drawing.Point(217, 206);
+            this.ageIn.Name = "ageIn";
+            this.ageIn.Size = new System.Drawing.Size(125, 27);
+            this.ageIn.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Age:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(74, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 41);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Blood Pressure Predictor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(134, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Please Enter age between 40 to 75";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(85, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(317, 51);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "This is a convenient application for quickly estimating\r\nyour middle-aged/elderly" +
+    " patient\'s blood pressure,\r\ngiven their age and weight in lbs.";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(134, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Please Enter Weight in pounds(lbs)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(114, 270);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Enter Weight:";
+            // 
+            // weightIn
+            // 
+            this.weightIn.Location = new System.Drawing.Point(217, 267);
+            this.weightIn.Name = "weightIn";
+            this.weightIn.Size = new System.Drawing.Size(125, 27);
+            this.weightIn.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(159, 378);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Result:";
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(217, 375);
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(125, 27);
+            this.result.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(491, 437);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.weightIn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ageIn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Label label1;
+        private TextBox ageIn;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox weightIn;
+        private Label label8;
+        private TextBox result;
+        private Button button1;
     }
 }
--- Regression/Regression/Form1.cs
+++ Regression/Regression/Form1.cs
@@ -6,5 +6,38 @@ namespace Regression
         {
             InitializeComponent();
         }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            TextBox inAge = (TextBox)ageIn;
+            TextBox inWieght = (TextBox)weightIn;
+            
+            
+            int a = 0;
+            int b = 0;
+            int ans = 0;
+
+            try
+            {
+                int weight = Convert.ToInt32(inWieght.Text);
+                int age = Convert.ToInt32(inAge.Text);
+                if (age >= 40 && age <= 75)
+                {
+
+                    result.Text = ans.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter Age between 40 and 75", "Notice:", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Please Enter Age And weight", "Error!:", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+
+        }
     }
 }
