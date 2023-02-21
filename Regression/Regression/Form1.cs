6c943d85dc55cd8c48ab0fa8db06c79c335028a6
adrianjaybarcenilla <adrianjay.barcenilla@cit.edu>
2023-02-21 16:58:11 +0800

    Add documentation

--- Regression/Regression/Form1.cs
+++ Regression/Regression/Form1.cs
@@ -13,12 +13,16 @@ namespace Regression
             TextBox inAge = (TextBox)ageIn;
             TextBox inWieght = (TextBox)weightIn;
 
+            // Independent variables (x1, x2)
             double weight = Double.Parse(inWieght.Text);
             double age = Double.Parse(inAge.Text);
 
+            // Intercept
             double a = 30.99410295;
+
             double b1 = 0.861414686;
             double b2 = 0.334859197;
+
             double ans = a + ((b1 * age) + (b2 * weight));
 
             try
